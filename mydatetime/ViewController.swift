27ee37b83adaa0e7704e82d20ac5bdcdecc3da4c
yosi <yosi@h-MBP-sl-yosi.lan>
2021-12-09 18:07:48 +0200

    start progect

---
+++ mydatetime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mydatetime
+//
+//  Created by yosi on 09/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
